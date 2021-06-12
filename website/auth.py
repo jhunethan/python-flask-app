e7098fd016aca2e0ed80df5aee4256a90c985341
jhunethan <jhunethan23@gmail.com>
2021-06-12 13:40:49 +0100

    rendered templates for login.html and sign_up.html

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return"<p>Login</p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return"<p>Logout</p>"
 
 @auth.route('/sign-up')
-def sing_up():
-    return"<p>Sign Up</p>"
+def sign_up():
+    return render_template("sign_up.html")
